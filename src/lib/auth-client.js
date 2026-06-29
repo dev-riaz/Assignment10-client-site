22109b5b35fc5628ec580e2611237cfc428dea5e
riaz113 <riazmahamud113@gmail.com>
2026-06-29 23:01:27 +0600

    solved some issue

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://assignment10-client-site-rcvy.vercel.app",
 });
 
 export const { signIn, signUp, signOut, useSession } = authClient;
