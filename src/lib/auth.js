7f37a1e9e605c9f8310b16bf71eee3c7473c578a
riaz113 <riazmahamud113@gmail.com>
2026-07-01 23:59:32 +0600

    solved diploy issue

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -8,11 +8,11 @@ const db = client.db("assignment10");
 export const auth = betterAuth({
     baseURL: process.env.BETTER_AUTH_URL,
     secret: process.env.BETTER_AUTH_SECRET,
-    trustedOrigins: [
-        "http://localhost:3000",
-        "https://assignmet10-client-site-rcvy.vercel.app",
-        "https://*.vercel.app",
-    ],
+    // trustedOrigins: [
+    //     "http://localhost:3000",
+    //     "https://assignmet10-client-site-rcvy.vercel.app",
+    //     "https://*.vercel.app",
+    // ],
     database: mongodbAdapter(db, {
         client,
     }),
@@ -31,6 +31,3 @@ export const auth = betterAuth({
     },
 });
 
-console.log("MONGO_DB_URI:", process.env.MONGO_DB_URI);
-console.log("BETTER_AUTH_URL:", process.env.BETTER_AUTH_URL);
-console.log("BETTER_AUTH_SECRET:", process.env.BETTER_AUTH_SECRET);
