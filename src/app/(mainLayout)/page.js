7d9f3dd709c99bc746e7ccbea79f75cd07507627
riaz113 <riazmahamud113@gmail.com>
2026-07-02 12:06:09 +0600

    solved some issue

--- src/app/(mainLayout)/page.js
+++ src/app/(mainLayout)/page.js
@@ -1,6 +1,6 @@
 import Banner from "@/components/Banner";
 import FeaturedRecipe from "@/components/FeaturedRecipe";
-
+// import FeaturedRecipe from "@/components/FeaturedRecipe";
 
 import RecipeHubWorks from "@/components/RecipeHubWorks";
 import Subscription from "@/components/Subscription";
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div>
       <Banner></Banner>
-      <FeaturedRecipe></FeaturedRecipe>
+     <FeaturedRecipe></FeaturedRecipe>
       <UserPopularRecipe></UserPopularRecipe>
       <RecipeHubWorks></RecipeHubWorks>
       <Subscription></Subscription>
